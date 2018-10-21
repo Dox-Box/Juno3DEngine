81fb6352fa67c855e511af5b28e40fbff73366f7
Jim Dox <43254987+DoxBox9@users.noreply.github.com>
2018-10-20 22:42:36 -0400

    physics update

--- Juno3DEngine-master/src/environment/Planet.java
+++ Juno3DEngine-master/src/environment/Planet.java
@@ -4,7 +4,8 @@ import org.joml.Vector3f;
 import graphicsEngine.Obj;
 
 public class Planet extends PhysicsObject {
-	
+	static final float bigG =  6.674 * Math.exp(10,-11);
+
 	public Planet(Obj texturedObj, Vector3f position, float rotX, float rotY, float rotZ, float scale) {
 		super(texturedObj, position, rotX, rotY, rotZ, scale);
 
@@ -16,4 +17,8 @@ public class Planet extends PhysicsObject {
 	public void setPlanetRotation(Vector3f vec) {
 		super.setAngularVel(vec);
 	}
+
+        public float getGravAccel(float distance){
+                (bigG * this.mass)/ (distance*distance);
+        }
 }
